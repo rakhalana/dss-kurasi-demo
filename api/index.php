39bacec596d19660b03b69d7c4e72e33e2bfb383
maollanaa <rakharfd123@gmail.com>
2026-06-16 18:55:13 +0700

    force APP_DEBUG true in api/index.php

--- api/index.php
+++ api/index.php
@@ -13,6 +13,10 @@ $_ENV['APP_STORAGE'] = $storagePath;
 $_SERVER['APP_STORAGE'] = $storagePath;
 putenv("APP_STORAGE={$storagePath}");
 
+$_ENV['APP_DEBUG'] = 'true';
+$_SERVER['APP_DEBUG'] = 'true';
+putenv("APP_DEBUG=true");
+
 $_ENV['VIEW_COMPILED_PATH'] = $storagePath . '/framework/views';
 $_SERVER['VIEW_COMPILED_PATH'] = $storagePath . '/framework/views';
 putenv("VIEW_COMPILED_PATH={$storagePath}/framework/views");
