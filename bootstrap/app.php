1f6ccbea34331a55a369d2f43220d4bf14b9c9a1
maollanaa <rakharfd123@gmail.com>
2026-06-16 18:36:27 +0700

    fix Laravel 11 ApplicationBuilder storage path error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,8 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->useStoragePath($_ENV['APP_STORAGE'] ?? $_SERVER['APP_STORAGE'] ?? base_path('storage'))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -17,3 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+$app->useStoragePath($_ENV['APP_STORAGE'] ?? $_SERVER['APP_STORAGE'] ?? $app->basePath('storage'));
+
+return $app;
